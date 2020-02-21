bbef0f6996d07ede79daddc3ac22871d18a2f898
Sam O-A <yibaah@gmail.com>
2020-02-21 10:46:07 -0500

    plus 2145

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("First test", () => {
+  expect(true).toBeTruthy();
+});
+
+test("Second test", () => {
+  expect(false).toBeFalsy();
 });
---
+++ src/Button.test.js
@@ -0,0 +1,71 @@
+import React from "react";
+//import { render, fireEvent } from "react-testing-library";
+//import Button from "./Button";
+
+test("object assignment", () => {
+  expect(2 + 2).toBe(4);
+});
+
+test("two plus two is four", () => {
+  const data = { one: 1 };
+  data["two"] = 2;
+  expect(data).toEqual({ one: 1, two: 2 });
+});
+
+test("adding positive numbers is not zero", () => {
+  for (let a = 1; a < 10; a++) {
+    for (let b = 1; b < 10; b++) {
+      expect(a + b).not.toBe(0);
+    }
+  }
+});
+
+test("null", () => {
+  const n = null;
+  expect(n).toBeNull();
+  expect(n).toBeDefined();
+  expect(n).not.toBeUndefined();
+  expect(n).not.toBeTruthy();
+  expect(n).toBeFalsy();
+});
+
+test("zero", () => {
+  const z = 0;
+  expect(z).not.toBeNull();
+  expect(z).toBeDefined();
+  expect(z).not.toBeUndefined();
+  expect(z).not.toBeTruthy();
+  expect(z).toBeFalsy();
+});
+
+test("two plus two", () => {
+  const value = 2 + 2;
+  expect(value).toBeGreaterThan(3);
+  expect(value).toBeGreaterThanOrEqual(3.5);
+  expect(value).toBeLessThan(5);
+  expect(value).toBeLessThanOrEqual(4.5);
+
+  //toBe and toEqual are equivalent for numbers
+  expect(value).toBe(4);
+  expect(value).toEqual(4);
+  expect(value).not.toBeFalsy();
+});
+
+test("adding floating point numbers", () => {
+  const value = 0.1 + 0.2;
+  //expect(value).toBe(0.3); this won't work because of round error
+  expect(value).toBeCloseTo(0.3); // This works.
+});
+
+test("there is no I in team", () => {
+  expect("team").not.toMatch(/I/);
+  expect("Christoph").toMatch(/stop/);
+});
+
+const shoppingList = [
+  "diapers",
+  "Kleenex",
+  "trash bags",
+  "paper towels",
+  "beer"
+];
